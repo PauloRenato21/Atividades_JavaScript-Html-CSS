19c4edfcd8f26637acf3758874e43e25658df0f3
PauloRenato21 <paulorenato401@gmail.com>
2020-10-29 12:09:13 -0300

    add 4 Questão

---
+++ 4_Atividade.js
@@ -0,0 +1,15 @@
+const n1 = 13;
+const n2 = 2;
+
+function divisao (n1,n2){
+    resultado = n1 / n2
+    return resultado
+}
+
+function resto (n1,n2){
+    resul = n1 % n2
+    return resul 
+}
+
+console.log(divisao (n1,n2))
+console.log(resto (n1,n2))
