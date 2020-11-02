f7ab2dd60f0d672e7cfa87eb97230cef0d852d4a
PauloRenato21 <paulorenato401@gmail.com>
2020-11-02 07:47:18 -0300

    parametros passados no console.log

--- 5_Atividade.js
+++ 5_Atividade.js
@@ -1,9 +1,7 @@
-const valor = 0.30000000000000004;
-
 function converter (valor){
-    valor.toFixed(2).replace(',,') // Converter o sinal de divisao das casas decimais
+    valor.toFixed(2).replace(',') // Converter o sinal de divisao das casas decimais
     resultado = valor.toLocaleString('pt-br', {style: 'currency', currency:'BRL'}) //Converter paara a moeda Real
     return resultado
 }
 
-console.log(converter (valor))
+console.log(converter (0.30000000000000004))
